263106e0f5c87703d0a9bd99c9ecdc39d53a986c
321-mark-123 <mark.first.name@gmail.com>
2022-02-28 18:09:54 +0100

    error

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
-    return render (request, 'chat/index.html')
+    return render(request, 'chat/index.html', {'username': 'Mark'})
--- first_django_app/first_django_app/urls.py
+++ first_django_app/first_django_app/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from chat.views import index
 from django.contrib import admin
 from django.urls import path
-# wurde auto insert from path('chat/', index),
-from first_django_app.chat.views import index
+
+""" wurde auto insert from path('chat/', index),
+# from first_django_app.chat.views import index #funktioniert nicht
+"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
