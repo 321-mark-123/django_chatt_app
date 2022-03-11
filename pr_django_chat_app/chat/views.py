ee990a82fa5883b938e5016b389b7a73d6e8d514
321-mark-123 <mark.first.name@gmail.com>
2022-03-11 15:25:30 +0100

    chat broken after add  requestPOST

--- pr_django_chat_app/chat/views.py
+++ pr_django_chat_app/chat/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Message
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'chat/index.html')
+    # print("received Data" + request.POST['textmessage'])
+    Message.objects.create(text=request.POST['textmessage'], chat=None, author=request.user, receiver=request.user
+                           )
+    return render(request, 'chat/index.html', {'username': 'mark'})
