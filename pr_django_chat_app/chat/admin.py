505fb8c9ee045b62abdd706455d3cf73b18fbfb1
321-mark-123 <mark.first.name@gmail.com>
2022-03-11 15:26:28 +0100

    d

--- pr_django_chat_app/chat/admin.py
+++ pr_django_chat_app/chat/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+# Register your models here
+from django.contrib import admin
+from .models import Chat, Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat', 'text', 'created_at', 'author', 'receiver')
+    list_display = ('created_at', 'author', 'text', 'receiver')
+    search_fields = ('text',)
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
