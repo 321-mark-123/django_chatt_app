a2621c50b40f37bc747e000da91395e8938bb81c
321-mark-123 <mark.first.name@gmail.com>
2022-03-12 11:59:40 +0100

    addLogin

--- pr_django_chat_app/chat/models.py
+++ pr_django_chat_app/chat/models.py
@@ -12,7 +12,7 @@ class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
     chat = models.ForeignKey(
-        Chat, on_delete=models.CASCADE, related_name='receiver_message_set', default=None, blank=True, null=True)
+        Chat, on_delete=models.CASCADE, default=None, blank=True, null=True)
 
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
--- pr_django_chat_app/chat/views.py
+++ pr_django_chat_app/chat/views.py
@@ -1,10 +1,36 @@
+from urllib import request
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Message
+from .models import Chat, Message
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+@login_required(login_url='/login')
 def index(request):
-    # print("received Data" + request.POST['textmessage'])
-    Message.objects.create(text=request.POST['textmessage'], chat=None, author=request.user, receiver=request.user
-                           )
-    return render(request, 'chat/index.html', {'username': 'mark'})
+    print(request.method)
+    if request.method == 'POST':
+        print("received Data" + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(
+            text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
+
+
+def login_view(request):
+    redirect = request.GET.get('next')
+    print('request.GET.get(next)', redirect)
+    if request.method == 'POST':
+
+        user = authenticate(username=request.POST.get(
+            'username'), password=request.POST.get('password'))
+        print(user)
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get('redirect'))
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
